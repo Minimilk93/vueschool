422fdf9d880cf06a0121067127e1f9104a33bfd5
Shelby Stanton <sstanton@helical-levity.com>
2018-10-08 17:35:33 +0100

    Adding forums to collections within categories exercise

---
+++ src/pages/PageCategory.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="col-full">
+    <div class="col-full push-top">
+      <h1>{{category.name}}</h1>
+      <CategoryListItem :category="category" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import CategoryListItem from '@/components/CategoryListItem'
+  import sourceData from '@/data.json'
+
+  export default {
+    components: {
+      CategoryListItem
+    },
+    props: {
+      id: {
+        required: true,
+        type: String
+      }
+    },
+    computed: {
+      category () {
+        return sourceData.categories[this.id]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
