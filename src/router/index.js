31ffe0bf57556b3478404eea5e8b1f9489b031bb
Shelby Stanton <shelby@Shelbys-MacBook-Pro.local>
2019-05-05 16:25:47 +0100

    Finishing chapter 7 work

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/pages/PageHome'
 import ThreadShow from '@/pages/PageThreadShow'
 import Forum from '@/pages/PageForum'
 import ThreadCreate from '@/pages/PageThreadCreate'
+import ThreadEdit from '@/pages/PageThreadEdit'
 import Category from '@/pages/PageCategory'
 import Profile from '@/pages/PageProfile'
 import NotFound from '@/pages/PageNotFound'
@@ -38,6 +39,12 @@ export default new Router({
       path: '/thread/:id',
       name: 'ThreadShow',
       component: ThreadShow,
+      props: true
+    },
+    {
+      path: '/thread/:id/edit',
+      name: 'ThreadEdit',
+      component: ThreadEdit,
       props: true
     },
     {
